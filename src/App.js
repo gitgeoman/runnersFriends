ba30c6f0b5e914f12439bc2d4bff781d379ffe7e
gitgeoman <gitgeoman@gmail.com>
2021-08-06 10:56:47 +0200

    update on map

--- src/App.js
+++ src/App.js
@@ -1,57 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 //my imports
-import GroupOfBlocks from './GroupOfBlock.js';
+import GroupOfBlocks from "./GroupOfBlock.js";
 //data import
-import {geojsonFeature} from './data/dane.js';
+import { geojsonFeature } from "./data/dane.js";
 
-import LeafletMap from './LeafletMap';
-import 'tachyons';
-import SearchBox from './SearchBox';
+import LeafletMap from "./LeafletMap";
+import "tachyons";
+import SearchBox from "./SearchBox";
 
-class App extends Component{
-  constructor(){
-    super()
-    this.state={
-        searchfield:'',
-        obiekty: geojsonFeature.features,
-        hoverID:'',
-        toggle:false
-
-    }
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchfield: "",
+      obiekty: geojsonFeature.features,
+      hoverID: "",
+      toggle: false,
+    };
   }
 
-  onSearchField=(event)=>{
-      this.setState({searchfield:event.target.value})
-
+  onSearchField = (event) => {
+    this.setState({ searchfield: event.target.value });
   };
 
-  onMouseHover=(data)=>{
-      this.setState({hoverID:(data.target.id)?data.target.id:''})
-      this.setState({toggle:true});
-      console.log(data.target.id)
+  onMouseHover = (data) => {
+    this.setState({ hoverID: data.target.id ? data.target.id : "" });
+    this.setState({ toggle: true });
+    //console.log(data.target.id);
   };
-  onMouseHoverEnd=(data)=>{
-    this.setState({toggle:false});
-    console.log('false')
+  onMouseHoverEnd = (data) => {
+    this.setState({ toggle: false });
+    console.log("false");
   };
-  render(){
-    const filteredData = this.state.obiekty.filter((obiekty)=>{
-      return obiekty.properties.description.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    } 
-    )
-//   console.log(filteredData);
+  render() {
+    const filteredData = this.state.obiekty.filter((obiekty) => {
+      return obiekty.properties.description.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    //   console.log(filteredData);
     return (
-      <div >
-        <div id='logo Nazwa'className='vh-25 f1 '>Funky name</div>
-        <SearchBox searchChangeAtribute={this.onSearchField}/>
-        <div className='dib w-100'>
-          <GroupOfBlocks obiekty={filteredData} onClickEvent={this.onMouseHover} onOut={this.onMouseHoverEnd} />
-          <LeafletMap obiekty={filteredData} toggle={this.state.toggle} hoverID={this.state.hoverID}/>
+      <div>
+        <div id="logo Nazwa" className="vh-25 f1 ">
+          Funky name
+        </div>
+        <SearchBox searchChangeAtribute={this.onSearchField} />
+        <div className="dib w-100">
+          <GroupOfBlocks
+            obiekty={filteredData}
+            onClickEvent={this.onMouseHover}
+            onOut={this.onMouseHoverEnd}
+          />
+          <LeafletMap
+            obiekty={filteredData}
+            toggle={this.state.toggle}
+            hoverID={this.state.hoverID}
+          />
         </div>
       </div>
-    )  
+    );
   }
-} 
+}
 
 export default App;
--- src/LeafletMap.js
+++ src/LeafletMap.js
@@ -1,109 +1,154 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 //import bibliotek leaflet
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 //naprawa zwalonych ikonek
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-
-
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import icon1 from "./data/img/SHGPUCAT--ED.svg";
+import icon2 from "./data/img/SFGPUCAT--ED.svg";
+import icon3 from "./data/img/SHGP------AH (1).svg";
 
 let DefaultIcon = L.icon({
-            iconUrl: icon,
-            shadowUrl: iconShadow
-        });
-        L.Marker.prototype.options.icon = DefaultIcon;
+  iconUrl: icon1,
+  shadowUrl: iconShadow,
+});
+
+let MasterIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [50, 50], // size of the icon
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 class LeafletMap extends Component {
   componentDidMount() {
     // create map
-    this.map = L.map('map', {
-      center: [52.29756190868707,21.250407],
+    this.map = L.map("map", {
+      center: [52.29756190868707, 21.250407],
       zoom: 8,
       layers: [
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+          attribution:
+            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         }),
-      ]
+      ],
     });
-    
+
+    //z geoservera
+    const URL = `http://localhost:8080/geoserver/AMyWorkspace/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=AMyWorkspace%3Apec015&maxFeatures=50&outputFormat=application%2Fjson`;
+
+    const oddajObiekt = (aoo) => {
+      console.log(aoo);
+      switch (aoo) {
+        case 999:
+          return {
+            iconUrl: icon1,
+            iconSize: [30, 30],
+          };
+        case 90:
+          return {
+            iconUrl: icon2,
+            iconSize: [30, 30],
+          };
+        case 190:
+          return {
+            iconUrl: icon3,
+            iconSize: [100, 100],
+          };
+        default:
+          return {
+            iconUrl: icon3,
+            iconSize: [20, 20],
+          };
+      }
+    };
+
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => {
+        L.geoJSON(data, {
+          pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+              icon: L.icon(oddajObiekt(feature.properties.aoo)),
+            });
+          },
+        }).addTo(this.map);
+      });
+
+    //koniec z geoservera
+
     //obiekt geojson przepisany do zmiennej
     let geodata = this.props.obiekty; // tak odwołuję się do obiektu który jest wysłany przez rodzica
     let popupOBJECTID = this.props.hoverID;
     let toggle = this.props.toggle;
-    console.log(toggle);
-    
+    //console.log(toggle);
 
-    this.layerMarkers=L.geoJSON(geodata , {
-        pointToLayer: function (feature, latlng) {//wstawia marker w postaci okręgu
-            return L.circleMarker(latlng
-            ).bindPopup(feature.properties.description.name)
-            //.on('mouseover', function(e){this.openPopup()})
-            //.on('mouseout', function(e){this.closePopup()})
-            ;
-            }
-      }
-      ).addTo(this.map);
-
+    this.layerMarkers = L.geoJSON(geodata, {
+      pointToLayer: function (feature, latlng) {
+        //wstawia marker w postaci okręgu
+        return L.circleMarker(latlng).bindPopup(
+          feature.properties.description.name
+        );
+        //.on('mouseover', function(e){this.openPopup()})
+        //.on('mouseout', function(e){this.closePopup()})
+      },
+    }).addTo(this.map);
   } //koniec Component didMount
 
-    updateMarkers(markersData) {
-      this.layerMarkers.clearLayers();
-      this.layerMarkers = L.geoJSON(markersData , {
-          pointToLayer: function (feature, latlng) 
-            {//wstawia marker w postaci okręgu
-              return L.circleMarker(latlng, 
-              ).bindPopup(feature.properties.description.name);
-              }
-        }
-       )
-     .addTo(this.map);
+  updateMarkers(markersData) {
+    this.layerMarkers.clearLayers();
+    this.layerMarkers = L.geoJSON(markersData, {
+      pointToLayer: function (feature, latlng) {
+        //wstawia marker w postaci okręgu
+        return L.circleMarker(latlng).bindPopup(
+          feature.properties.description.name
+        );
+      },
+    }).addTo(this.map);
+  }
+
+  createMarkers(hoveredMarker) {
+    //czyszczenie warstwy z markerami
+    //tworzenie nowej warstwy z markerami
+    if (this.specialLayer) {
+      this.specialLayer.clearLayers();
     }
+    this.specialLayer = L.geoJSON(hoveredMarker, {
+      pointToLayer: function (feature, latlng) {
+        //wstawia marker w postaci okręgu
+        return L.circleMarker(latlng, {
+          radius: 8,
+          fillColor: "#ff7800",
+          color: "#000",
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.8,
+        }).bindPopup(feature.properties.description.name);
+      },
+    }).addTo(this.map);
+  }
 
-    createMarkers(hoveredMarker){
-      //czyszczenie warstwy z markerami
-      //tworzenie nowej warstwy z markerami
-          if(this.specialLayer){
-            this.specialLayer.clearLayers();
-          }
-          this.specialLayer = L.geoJSON(hoveredMarker , {
-          pointToLayer: function (feature, latlng) 
-            {//wstawia marker w postaci okręgu
-              return L.circleMarker(latlng, {
-                                                radius: 8,
-                                                fillColor: "#ff7800",
-                                                color: "#000",
-                                                weight: 1,
-                                                opacity: 1,
-                                                fillOpacity: 0.8
-                                            }
-              ).bindPopup(feature.properties.description.name);
-              }
+  componentDidUpdate({ popupOBJECTID, geodata, toggle }) {
+    // -->> sprawdzam co sie zmieniło w danych
+    if (this.props.toggle !== toggle || this.props.obiekty !== geodata) {
+      //jezeli zmienil sie toggle lub zmienil sie input data
+      this.updateMarkers(this.props.obiekty); //aktualizuj obiekty
+      if (this.props.hoverID !== popupOBJECTID && this.props.hoverID !== "") {
+        //jezeli najechano na hovera i hover ma wartosc to
+        this.createMarkers(this.props.obiekty[Number(this.props.hoverID) - 1]);
+      }
+      if (this.props.toggle === false) {
+        if (this.specialLayer) {
+          this.specialLayer.clearLayers();
         }
-        ).addTo(this.map);
+      }
     }
-
-    componentDidUpdate({popupOBJECTID, geodata, toggle}) {
-    // -->> sprawdzam co sie zmieniło w danych 
-        if (this.props.toggle !== toggle || this.props.obiekty !== geodata){//jezeli zmienil sie toggle lub zmienil sie input data
-           this.updateMarkers(this.props.obiekty); //aktualizuj obiekty
-           if (this.props.hoverID !== popupOBJECTID && this.props.hoverID!=''){ //jezeli najechano na hovera i hover ma wartosc to
-               this.createMarkers(this.props.obiekty[Number(this.props.hoverID)-1])
-           }
-           if (this.props.toggle==false){
-              if(this.specialLayer){
-                  this.specialLayer.clearLayers();
-              }
-           }
-        }
-    }//koniec componetdidupdate
-
+  } //koniec componetdidupdate
 
   render() {
-    return <div className='fl w-50 pa2 vh-75' id="map"></div>
+    return <div className="fl w-50 pa2 vh-75" id="map"></div>;
   }
 }
 export default LeafletMap;
